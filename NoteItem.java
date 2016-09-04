23a841e717bc3865da36468ccfe430765be5bea2
sarasneedmiller <sesneed@gmail.com>
2016-09-04 12:33:37 -0500

    Create file NoteItem.java
    This is where a NoteItem is defined

---
+++ NoteItem.java
@@ -0,0 +1,48 @@
+package com.sneedmiller.juliet.com.sneedmiller.juliet.plainolnotes.data;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Juliet on 7/30/2016.
+ */
+public class NoteItem {
+
+    private String key;
+    private String text;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public static NoteItem getNew() {
+
+        Locale locale = new Locale("en_US");
+        Locale.setDefault(locale);
+
+        String pattern = "yyyy-MM-dd HH:mm:ss Z";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        String key = formatter.format(new Date());
+
+        NoteItem note = new NoteItem();
+        note.setKey(key);
+        note.setText("");
+        return note;
+
+    }
+
+}
